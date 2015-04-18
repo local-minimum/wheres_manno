e3b9c767079a6db938b52a98f46114e98fd6a8ae
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-18 14:27:00 +0200

    Basic rambler implemented

--- Assets/Scripts/Rambler.cs
+++ Assets/Scripts/Rambler.cs
@@ -3,13 +3,52 @@ using System.Collections;
 
 public class Rambler : MonoBehaviour {
 
+	[SerializeField] float timeBetweenRambles = 2f;
+	[Range(0, 1)][SerializeField] float timeBetweenVariability = 0.1f;
+	AudioSource player;
+	float nextRamble;
+	Story story;
+	bool touching = false;
+
 	// Use this for initialization
 	void Start () {
-	
+		player = GetComponentInChildren<AudioSource>();
+		story = GameObject.FindObjectOfType<Story>();
+		if (player == null)
+			player = gameObject.AddComponent<AudioSource>();
+		nextRamble = Random.Range(-timeBetweenRambles, timeBetweenRambles);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (RambleTime)
+			Ramble();
+	}
+
+	bool RambleTime {
+		get {
+			return touching && !player.isPlaying && Time.timeSinceLevelLoad > nextRamble; 
+		}
+	}
+
+	void Ramble() {
+		player.PlayOneShot(story.NextRambling);
+		Debug.Log("Ramble");
+		SetNextRambleTime();
+	}
+
+	void SetNextRambleTime() {
+		nextRamble = Time.timeSinceLevelLoad +
+			timeBetweenRambles * (1 + Random.Range(-timeBetweenVariability, timeBetweenVariability));
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+			touching = true;
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player")
+			touching = false;
 	}
 }
