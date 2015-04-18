d87f3329f41321b3bfcbc29952766e87a25285bb
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-18 15:09:21 +0200

    Better walk speed and more sickness effects

---
+++ Assets/Scripts/Ailments/Darkness.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class Darkness : Ailment {
+
+	VignetteAndChromaticAberration effect;
+	Tonemapping effect2;
+	float minValue;
+	float baseExposure;
+	[SerializeField] float maxValue = 10f;
+
+	// Use this for initialization
+	void Start () {
+		effect = GetComponentInChildren<VignetteAndChromaticAberration>();
+		effect2 = GetComponentInChildren<Tonemapping>();
+		minValue = effect.intensity;
+		baseExposure = effect2.exposureAdjustment;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		effect.intensity = Mathf.Lerp(minValue, maxValue, intensity);
+		effect2.exposureAdjustment = baseExposure - Mathf.Pow(intensity, 2f);
+	}
+}
---
+++ Assets/Scripts/Ailments/Occlusion_Worries.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class Occlusion_Worries : Ailment {
+
+	ScreenSpaceAmbientOcclusion effect;
+	float maxEffect;
+
+	// Use this for initialization
+	void Start () {
+		effect = GetComponentInChildren<ScreenSpaceAmbientOcclusion>();
+		maxEffect = effect.m_OcclusionIntensity;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		effect.m_OcclusionIntensity = maxEffect * Mathf.Pow(intensity, 2);
+	}
+}
