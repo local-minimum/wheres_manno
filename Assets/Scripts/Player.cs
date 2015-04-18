c1ff30a56631169f8596fce613dbe9511366f317
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-18 17:42:53 +0200

    Sleeps brings character back to bed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	Story story;
+
+	Vector3 startPosition;
+	Quaternion startRotation;
+	Vector3 startScale;
+
+	void Awake() {
+		story = GameObject.FindObjectOfType<Story>();
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		startPosition = transform.localPosition;
+		startRotation = transform.localRotation;
+		startScale = transform.localScale;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnEnable() {
+		story.OnSleep += HandleSleep;
+	}
+
+	void OnDisable() {
+		story.OnSleep -= HandleSleep;
+	}
+
+	void HandleSleep(SleepTypes sleepType) {
+		if (sleepType == SleepTypes.FoundImposter || sleepType == SleepTypes.GotTired)
+			ResetStartValues();
+	}
+
+	void ResetStartValues() {
+		transform.localPosition = startPosition;
+		transform.localScale = startScale;
+		transform.localRotation = startRotation;
 	}
 }
