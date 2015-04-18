f664b34c9207cd714f92d6d5e5c2d6bdcee2eb28
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-18 12:01:12 +0200

    Vortex with intensity slider

---
+++ Assets/Scripts/Ailments/Ailment.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ailment : MonoBehaviour {
+
+	[Range(0, 1)] [SerializeField] protected float intensity;
+	
+}
---
+++ Assets/Scripts/Ailments/Vortex_World.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class Vortex_World : Ailment {
+
+	Vortex vortex;
+	[SerializeField] float periodLower;
+	[SerializeField] float periodUpper;
+
+	float vortexAmplitude;
+	float angleTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		vortex = GetComponentInChildren<Vortex>();
+		vortexAmplitude = Mathf.Abs(vortex.angle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angleTime += Time.deltaTime * Mathf.Lerp(periodLower, periodUpper, intensity);
+		vortex.angle = Mathf.Cos(angleTime) * vortexAmplitude;
+	}
+}
