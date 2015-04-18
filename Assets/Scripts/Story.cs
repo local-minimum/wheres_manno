1e75391d4f4630eeb927c5f1b5ab7d40e79f7b51
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-18 14:16:17 +0200

    Extended story to have ramblings

--- Assets/Scripts/Story.cs
+++ Assets/Scripts/Story.cs
@@ -5,6 +5,8 @@ public class Story : MonoBehaviour {
 
 	[SerializeField] int playIteration = 0;
 	[SerializeField] AudioClip[] imposterVocalisations;
+	[SerializeField] AudioClip[] ramblings;
+	int rambling = -1;
 
 	public int PlayIteration {
 		get {
@@ -15,6 +17,17 @@ public class Story : MonoBehaviour {
 	public AudioClip ImposterVocalisation {
 		get {
 			return imposterVocalisations[Random.Range(0, imposterVocalisations.Length)];
+		}
+	}
+
+	public AudioClip NextRambling {
+		get {
+			if (ramblings.Length == 0)
+				return null;
+			rambling ++;
+			rambling %= ramblings.Length;
+
+			return ramblings[rambling];
 		}
 	}
 
