aee23fe659a65d9c2efcfa44831e84bac1b80568
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-19 19:41:02 +0200

    Turned off walktrail when player wins

---
+++ Assets/Scripts/WalkTrail.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkTrail : MonoBehaviour {
+
+	Story story;
+	Player player;
+	ParticleSystem emitter;
+
+	// Use this for initialization
+	void Awake () {
+		story = GameObject.FindObjectOfType<Story>();
+		player = GameObject.FindObjectOfType<Player>();
+		emitter = GetComponent<ParticleSystem>();
+	}
+
+	void OnEnable() {
+		story.OnPlayerSleep += HandleSleep;
+		story.OnPlayerWakeUp += HandleAwake;
+	}
+
+	void OnDisable() {
+		story.OnPlayerSleep -= HandleSleep;
+		story.OnPlayerWakeUp -= HandleAwake;
+	}
+
+	void HandleSleep(SleepTypes sleepType) {
+		if (sleepType == SleepTypes.FoundTarget) {
+			emitter.Clear();
+			emitter.Stop();
+
+		}
+	}
+
+	void HandleAwake(SleepTypes sleepType) {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
