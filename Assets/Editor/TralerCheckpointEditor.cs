7724520dd08accf276a9d9ca2e55d6b124dc80f2
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-26 11:32:52 +0200

    Light trail checkpoints implemented

---
+++ Assets/Editor/TralerCheckpointEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Checkpoint))]
+public class TralerCheckpointEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		Checkpoint mytarget = (Checkpoint) target;
+		base.OnInspectorGUI ();
+		if (GUILayout.Button("Automatic edges")) {
+			mytarget.edges.AddRange(mytarget.GetAutoDiscovery());
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
---
+++ Assets/Scripts/Trailer/Checkpoint.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Checkpoint : MonoBehaviour {
+
+	static HashSet<Checkpoint> nodes = new HashSet<Checkpoint>();
+
+	[SerializeField] float autoDiscoverRadius = 2f;
+
+	public List<Checkpoint> edges = new List<Checkpoint>();
+
+	public List<Checkpoint> GetAutoDiscovery() {
+		List<Checkpoint> autoDiscovery = new List<Checkpoint>();
+		foreach (Checkpoint node in Checkpoint.nodes) {
+
+			if (!edges.Contains(node) && node != this &&
+			    Vector3.Distance(transform.position, node.transform.position) < autoDiscoverRadius) {
+
+				autoDiscovery.Add(node);
+			}
+		}
+
+		return autoDiscovery;
+	}
+
+	void OnEnable() {
+		Checkpoint.nodes.Add(this);
+	}
+
+	void OnDisable() {
+		Checkpoint.nodes.Remove(this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (edges.Count == 0)
+			GetAutoDiscovery();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos() {
+		foreach (Checkpoint pt in edges)
+			Gizmos.DrawLine(transform.position, pt.transform.position);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, autoDiscoverRadius);
+		foreach (Checkpoint pt in edges)
+			Gizmos.DrawLine(transform.position, pt.transform.position);
+	}
+}
