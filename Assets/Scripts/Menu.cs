8711dd23bec5a8848a546d1d87c1fd3b4992d0fb
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-05-12 20:08:08 +0200

    Tweaking menu and light trail

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,12 +4,29 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 
 	[SerializeField] string playScene;
-	
+	[SerializeField] string helpTrigger;
+	[SerializeField] Animator animations;
+
+	void OnEnable() {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Cancel"))
-			Application.Quit();
-		else if (Input.anyKeyDown)
-			Application.LoadLevel(playScene);
+			Exit();
+	}
+
+	public void Play() {
+		Application.LoadLevel(playScene);
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+
+	public void Help() {
+		animations.SetTrigger(helpTrigger);
 	}
 }
---
+++ Assets/Scripts/Pendelum.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pendelum : MonoBehaviour {
+
+	[SerializeField] float y;
+	[SerializeField] float rotationTime;
+
+	// Use this for initialization
+	void Start () {
+		iTween.RotateTo(
+			gameObject,
+			iTween.Hash(
+				"y", y,
+				"time", rotationTime,
+				"easetype", iTween.EaseType.easeInOutCubic,
+				"looptype", iTween.LoopType.pingPong
+			));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
